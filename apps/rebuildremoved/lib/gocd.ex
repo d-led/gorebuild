1d42ae2967aded391726ee8fc5917b492948f9ae
Dmitry Ledentsov <d-led@users.noreply.github.com>
2017-12-08 23:58:01 +0100

    noise

--- apps/rebuildremoved/lib/gocd.ex
+++ apps/rebuildremoved/lib/gocd.ex
@@ -42,7 +42,6 @@ defmodule Gocd do
 
     # implementation
 
-    # {:ok, %Tesla.Env{__client__: nil, __module__: Gocd, body: [%{"files" => [%{"name" => "config.exs", "type" => "file", "url" => "https://localhost:8154/go/files/test/1/defaultStage/1/defaultJob/config/config.exs"}], "name" => "config", "type" => "folder", "url" => "https://localhost:8154/go/files/test/1/defaultStage/1/defaultJob/config"}, %{"files" => [%{"name" => "console.log", "type" => "file", "url" => "https://localhost:8154/go/files/test/1/defaultStage/1/defaultJob/cruise-output/console.log"}, %{"name" => "md5.checksum", "type" => "file", "url" => "https://localhost:8154/go/files/test/1/defaultStage/1/defaultJob/cruise-output/md5.checksum"}], "name" => "cruise-output", "type" => "folder", "url" => "https://localhost:8154/go/files/test/1/defaultStage/1/defaultJob/cruise-output"}, %{"files" => [%{"files" => [%{"name" => "start.sh", "type" => "file", "url" => "https://localhost:8154/go/files/test/1/defaultStage/1/defaultJob/foo/bar/start.sh"}], "name" => "bar", "type" => "folder", "url" => "https://localhost:8154/go/files/test/1/defaultStage/1/defaultJob/foo/bar"}], "name" => "foo", "type" => "folder", "url" => "https://localhost:8154/go/files/test/1/defaultStage/1/defaultJob/foo"}] ...
     defp artifacts_of_latest_run(%{pipeline: pipeline, stage: stage, job: job}) do
         { :ok, %Tesla.Env{body: files} } = get("/files"<>"/#{pipeline}"<>"/Latest"<>"/#{stage}"<>"/Latest"<>"/#{job}"<>".json")
         files
